2bca5502ba0f714666e9fcc60841cce847c3768f
root <root@Win-SK.localdomain>
2017-08-03 11:05:13 +0200

    Minimo

---
+++ Minimo_aproximado_funcion.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double Ecuacion(const double& x){
+    return 5 * x * x - 7 * x - 13;
+}
+
+int main(){
+    double valorInicial = rand() % 10;
+    double Yini = Ecuacion(valorInicial);
+    double variacion = 1;
+    double Ysigue;
+
+    while(abs(variacion) > 0.00001){
+        Ysigue = Ecuacion(valorInicial + variacion);
+
+        if(Ysigue > Yini){      // No disminuye, seguimos hacia un paso menor
+            variacion *= -1;    // Cambiamos el signo de la variacion
+            variacion /= 10;    // Reducimos la variacion
+        }
+        else{
+            Yini = Ysigue;      // Hallamos un valor menor que el anterior y disminuimos a este nuevo valor
+            valorInicial += variacion;
+            cout << "x: " << valorInicial << " + Yini: " << Yini << endl;
+        }
+    }
+
+    cout << "Respuesta: " << valorInicial << endl;
+
+    return 0;
+}
